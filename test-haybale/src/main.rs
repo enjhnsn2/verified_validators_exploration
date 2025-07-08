d48178f3462c8de016eb7a68b50cc3283b7655a5
Evan Johnson <e5johnso@ucsd.edu>
2025-07-08 13:53:34 -0700

    take commandline arguments instead of harcoding binary path and func name

--- test-haybale/src/main.rs
+++ test-haybale/src/main.rs
@@ -1,7 +1,18 @@
 use haybale::{Config, Project, backend::DefaultBackend, symex_function};
 mod hooks;
 mod types;
+use clap::Parser;
 use hooks::add_hooks;
+
+/// Command-line arguments
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the LLVM bitcode file (binary)
+    binary: String,
+    /// Name of the function to symbolically execute
+    function: String,
+}
 
 fn symex_func_with_loop_bound(func_name: &str, project: &Project, loop_bound: usize) {
     let mut config: Config<DefaultBackend> = Config::default();
@@ -29,10 +40,9 @@ fn symex_func_with_loop_bound(func_name: &str, project: &Project, loop_bound: us
 
 fn main() {
     env_logger::init();
-    // Maps function calls to symbolic return values
-    // hook for (*array[0] = ...)
-    // try to add hook for precondition
+    let args = Args::parse();
+    let loop_bound = 1000;
 
-    let project = Project::from_bc_path("../examples/host.bc").unwrap();
-    symex_func_with_loop_bound("sandbox_array_index_checked", &project, 1000);
+    let project = Project::from_bc_path(&args.binary).unwrap();
+    symex_func_with_loop_bound(&args.function, &project, loop_bound);
 }
